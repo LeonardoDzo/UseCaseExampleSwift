6ce8dd2dbb9dd9930724cfb9993d18149fc17641
Leonardo Durazo <leonardo.durazo@geopagos.com>
2021-09-13 10:56:30 -0700

    init commit

---
+++ UseCasesExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UseCasesExample
+//
+//  Created by Leonardo Durazo on 13/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UseCasesExample/UseCasesExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UseCasesExampleApp.swift
+//  UseCasesExample
+//
+//  Created by Leonardo Durazo on 13/09/21.
+//
+
+import SwiftUI
+
+@main
+struct UseCasesExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
